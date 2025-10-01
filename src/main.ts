81e2c05bd9d4eba65118c9b00974d5cd996ec58a
Visar Domi <visardomi4@gmail.com>
2025-10-01 21:53:04 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 // src/main.ts
 import { DownloaderService } from "./downloader/downloaderService.js";
-import { GrowerService } from "./grower/growerService.js";
 
 async function main() {
     const downloaderService = await DownloaderService.create();
     downloaderService.start();
-
-    const growerService = new GrowerService();
-    growerService.start();
 }
 
 main();
