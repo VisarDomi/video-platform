65e25ed895d80dfdd8b012434dd70b2c374e6283
Visar Domi <visardomi4@gmail.com>
2025-10-02 19:17:56 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 // src/main.ts
 import "dotenv/config";
 
-import logger from "./common/logger.js";
 import { AuthService } from "./auth/authService.js";
 
 async function main() {
-    logger.info("--- Starting Tango Auth Service ---");
     const authService = new AuthService();
     await authService.initiateAuth();
     authService.startBackgroundJobs();
